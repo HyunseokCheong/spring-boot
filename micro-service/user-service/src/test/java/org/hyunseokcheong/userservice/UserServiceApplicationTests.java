9caa0d84df82efa19a69604cec9848395bb0e498
HyunseokCheong <hyunseokcheong@gmail.com>
2024-03-11 13:48:52 +0900

    섹션 1. Service Discovery
    User Service - 프로젝트 생성

---
+++ micro-service/user-service/src/test/java/org/hyunseokcheong/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyunseokcheong.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
